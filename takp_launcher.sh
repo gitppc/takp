798228ff246bdcf1db24cd2af01be61f1f797604
gitppc <99992013+gitppc@users.noreply.github.com>
2022-02-21 09:44:10 -0600

    Update takp_launcher.sh

--- takp_launcher.sh
+++ takp_launcher.sh
@@ -142,7 +142,7 @@ export DXVK_HUD=fps
 #export DXVK_HUD=full
 
 install() {
-	sudo apt install wine xdotool libvulkan1:i386 p7zip-full
+	sudo apt install wine xdotool libvulkan1:i386
 	mkdir -p "${wine_prefix}"
 	# Some rudimentary caching
 	mkdir -p "${cache_base}"
